b327216f75aa9d2110866f3e5e6c9fb728ebe2f9
Jennings Zhang <dev@sl.jennin.xyz>
2024-10-09 09:40:02 -0400

    Replicate the bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { useImmer } from "use-immer";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [foo, setFoo] = useImmer([]);
+	const [bar, setBar] = useImmer([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const onClick = () => {
+		setFoo((draft) => {
+			draft.push(1);
+		});
+		console.count("onClick");
+		setBar((draft) => {
+			console.count("setBar");
+			draft.push(2);
+		});
+	};
+
+	React.useEffect(() => {
+		console.count("useEffect");
+	}, [bar]);
+
+	return <button onClick={onClick}>click me</button>;
 }
 
-export default App
+export default App;
